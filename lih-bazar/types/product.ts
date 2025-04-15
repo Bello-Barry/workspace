e9fe2bb92dc1d74e79d0a26959e946e9fb50c90c
Bello-Barry <79174154+Bello-Barry@users.noreply.github.com>
2025-04-15 14:33:26 +0100

    Update product.ts

--- lih-bazar/types/product.ts
+++ lih-bazar/types/product.ts
@@ -1,4 +1,18 @@
 // types/product.ts
+
+// types/product.ts
+export type Product = {
+  id: string;
+  name: string;
+  price: number;
+  stock: number;
+  images: string[] | string;
+  description?: string;
+  metadata?: Record<string, any>; // <-- Ajoutez cette ligne
+  // ... autres propriétés
+};
+
+
 export interface Product {
   id: string;
   name: string;
