b3c5968db915a358629dfb10b415e2ae114aad95
Bello-Barry <79174154+Bello-Barry@users.noreply.github.com>
2025-04-15 14:50:06 +0100

    Update product.ts

--- lih-bazar/types/product.ts
+++ lih-bazar/types/product.ts
@@ -13,16 +13,6 @@ export type Product = {
 };
 
 
-export interface Product {
-  id: string;
-  name: string;
-  description: string;
-  price: number;
-  stock: number;
-  images: string; // Stocké comme string séparée par des virgules
-  category: string;
-  created_at: string;
-}
 
 export interface Category {
   name: string;
